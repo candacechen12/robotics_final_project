7128b5692fb39be6b93fd3b466e0303fc78495c8
sususuint <113937210+sususuint@users.noreply.github.com>
2022-11-29 20:46:22 -0500

    Add files via upload

---
+++ local_planner.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+
+# Suhayla Ahmed (sia3yd)
+# The purpose of the local_planner is to avoid obstacles that are not yet on the map.
+# The exact behavior will depend on your implementation of the global planner;
+# however, the local planner must act as a runtime monitor to monitor for
+# imminent collisions and prevent them. You may implement this however you like,
+# using what you’ve learned from previous labs.
+
+class LocalPlanner():
+  # On node initialization
+  def __init__(self):
+    pass
