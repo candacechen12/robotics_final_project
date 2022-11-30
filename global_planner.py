4494c5e4841e5c8587d7de9fa71f11298a9f9455
candacechen12 <73714837+candacechen12@users.noreply.github.com>
2022-11-29 21:39:29 -0500

    Update global_planner.py

--- global_planner.py
+++ global_planner.py
@@ -1,67 +1,46 @@
-# !/usr/bin/env python
-
-# Suhayla Ahmed (sia3yd)
-# The purpose of the global_planner is to plan a path to a goal.
-# This planning must be done in a way that is cognizant of the unknown map
-# and explore in order to find the dog. You may implement the global planner however you like,
-# using what you’ve learned from the previous labs.
-
+#!/usr/bin/env python
 import rospy
+import time
+import copy
+from enum import Enum
+from math import sqrt, pow
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Point
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
-from Queue import PriorityQueue
-import math
+from tf2_geometry_msgs import do_transform_point
 
+class GlobalPlanner:
+    def __init__(self):
+        self.lidar_sub = rospy.Subscriber("/uav/sensors/lidar", LaserScan, self.processLidar, queue_size=1)
+        self.gps_sub = rospy.Subscriber("/uav/sensors/gps", PoseStamped, self.processGPS, queue_size=1)
 
-class GlobalPlanner():
-  # On node initialization
-  def __init__(self):
-    self.lidar_sub = rospy.Subscriber("/uav/sensors/lidar", LaserScan, self.processLidar, queue_size=1)
-    self.mainloop()
+        self.position = Vector3()
+        self.grid = OccupancyGrid()
 
-  def processLidar(self, msg):
-    rospy.loginfo(msg)
+        self.mainloop()
 
-  def plan(self, map_data, drone_position, dog_position):
-    self.validate_data(map_data, drone_position, dog_position)
-    map_data = self.expand_obstacles(map_data, self.safe_distance)
-    frontier = PriorityQueue()
-    frontier.put((0, drone_position))
-    came_from = {}
-    cost_so_far = {}
-    came_from[str(drone_position)] = None
-    cost_so_far[str(drone_position)] = 0
-    final_path = []
-    while not frontier.empty():
-      current = frontier.get()
-      current = list(current[1])
-      if current[0] == dog_position[0] and current[1] == dog_position[1]:
-          while came_from[str(current)] != None:
-              final_path.append(current)
-              current = came_from[str(current)]
-          final_path.append(current)
-          return list(reversed(final_path))
-      for next in self.get_neighbors(current, map_data):
-        edge_cost = math.sqrt(((current[0] - next[0])**2) + ((current[1] - next[1]) ** 2))
-        goal_cost = math.sqrt(((dog_position[0] - next[0])**2) + ((dog_position[1] - next[1]) ** 2))
-        new_cost = cost_so_far[str(current)] + edge_cost
-        if str(next) not in cost_so_far or new_cost < cost_so_far[str(next)]:
-            cost_so_far[str(next)] = new_cost
-            priority = new_cost + goal_cost
-            frontier.put((priority, next))
-            came_from[str(next)] = current
+    def processLidar(self, msg):
+      rospy.loginfo(msg)
+    
+    def processGPS(self, msg):
+      self.position.x = msg.pose.position.x
+      self.position.y = msg.pose.position.y
+    
+    
+    
+    # The main loop of the function
+    def mainloop(self):
+        # Set the rate of this loop
+        rate = rospy.Rate(20)
 
-    print("Error: No Path Found")
-    return [drone_position]
+        # While ROS is still running
+        while not rospy.is_shutdown():
+            pass
+        # Sleep for the remainder of the loop
+        rate.sleep()
 
-  # main loop
-  def mainloop(self):
-    rate = rospy.Rate(20)
-
-    while not rospy.is_shutdown():
-      pass
-
-    rate.sleep()
 
 if __name__ == '__main__':
   rospy.init_node('global_planner_node')
